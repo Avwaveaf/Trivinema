3bcd31dd88805c8566018cfec1035203151cc0b2
avwaveaf <avwave12@gmail.com>
2025-04-25 13:22:27 +0700

    Initial Commit

---
+++ Trivinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trivinema
+//
+//  Created by Afif Fadillah on 25/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
